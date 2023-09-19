249354ad45c23d729fde27f60ed7bf1eccb9b34b
AnaJuliaSilverio <ana.silverio@zup.com.br>
2023-09-19 14:16:10 -0300

    add fourth exercice

--- src/main/java/org/example/desafio1/Exercicio3.java
+++ src/main/java/org/example/desafio1/Exercicio3.java
@@ -2,27 +2,20 @@ package org.example.desafio1;
 
 import java.util.Scanner;
 
+
 public class Exercicio3 {
     public static void main(String[] args) {
-        try {
-            Scanner scanner = new Scanner(System.in);
-            System.out.println("Digite uma nota: ");
-            double grade = scanner.nextDouble();
-            checkGrade(grade);
-            String result = studentResult(grade);
-            System.out.println("O aluno está :"+result);
-        }catch (IllegalArgumentException exception){
-            System.out.println(exception.getMessage());
-        }
-
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite uma letra: ");
+        char letter= scanner.next().charAt(0);
+        String result = vowelOrConsonant(Character.toString(letter));
+        System.out.println("A letra  "+letter+" é uma "+result);
 
     }
-    public static String studentResult(double grade){
-        if (grade<6) return "Reprovado";
-        else if (grade>6 && grade<8) return "Em recuperação";
-        else return "Aprovado";
-    }
-    public static void checkGrade(double grade){
-        if (grade<0 || grade>10) throw new IllegalArgumentException("A nota deve estar entre 0 e 10");
+    public static String vowelOrConsonant(String letter){
+        String regex = "[aeiouAEIOU]";
+        if (letter.matches(regex))return "Vogal";
+        else return "Consoante";
+
     }
 }
