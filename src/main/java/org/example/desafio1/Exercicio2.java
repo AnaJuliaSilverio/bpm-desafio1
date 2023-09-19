92d1c3e4591500a4c983c441006c71c5b01d777d
AnaJuliaSilverio <ana.silverio@zup.com.br>
2023-09-19 13:48:29 -0300

    adding second exercice

---
+++ src/main/java/org/example/desafio1/Exercicio2.java
@@ -0,0 +1,19 @@
+package org.example.desafio1;
+
+import java.util.Scanner;
+
+public class Exercicio2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite um numero: ");
+        int number = scanner.nextInt();
+        String result = positiveOrNegative(number);
+        System.out.println("O valor "+number+" é "+result);
+
+    }
+    public static String positiveOrNegative(int number){
+        if (number>=0)return "Par";
+        else return "Impar";
+
+    }
+}
