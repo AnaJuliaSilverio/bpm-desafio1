0381fa85fad9fe5984e74db20978217de02fd968
AnaJuliaSilverio <ana.silverio@zup.com.br>
2023-09-19 13:44:20 -0300

    adding fisrt exercice

---
+++ src/main/java/org/example/desafio1/Exercicio1.java
@@ -0,0 +1,18 @@
+package org.example.desafio1;
+
+import java.util.Scanner;
+
+public class Exercicio1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite o valor do primeiro número: ");
+        int fisrtNumber = scanner.nextInt();
+        int secondNumber = scanner.nextInt();
+        int result = higherNumber(fisrtNumber,secondNumber);
+
+        System.out.println("O maior valor entre "+fisrtNumber+" e "+secondNumber+" é "+ result);
+    }
+    public static int higherNumber(int fisrtNumber,int secondNumber){
+        return Math.max(fisrtNumber, secondNumber);
+    }
+}
