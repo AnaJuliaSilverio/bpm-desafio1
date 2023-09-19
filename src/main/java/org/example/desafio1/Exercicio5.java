eb069731b7e23e8b08ca204d615fc31df6ea5d07
AnaJuliaSilverio <ana.silverio@zup.com.br>
2023-09-19 14:22:37 -0300

    add fifth exercice

---
+++ src/main/java/org/example/desafio1/Exercicio5.java
@@ -0,0 +1,24 @@
+package org.example.desafio1;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.Scanner;
+
+public class Exercicio5 {
+
+    public static void main(String[] args) {
+        ArrayList<Integer> numbers = new ArrayList<>();
+        numbers.add(5);
+        numbers.add(6);
+        numbers.add(0);
+        numbers.add(4);
+        ArrayList<Integer> sortingNumbers = sortingNumbers(numbers);
+        System.out.println("A lista ordenada de forma decrescente é "+ sortingNumbers);
+
+
+    }
+    public static ArrayList<Integer> sortingNumbers(ArrayList<Integer> numbers){
+        numbers.sort(Comparator.reverseOrder());
+        return numbers;
+    }
+}
